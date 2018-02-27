613f97e940c3229f1a74b00019822cba5c9b93df
Martin Larralde <martin.larralde@ens-cachan.fr>
2018-02-27 21:44:23 +0100

    Add setuptools configuration and setup file

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+import setuptools
+import setuptools_rust as rust
+
+setuptools.setup(
+    setup_requires=[
+        "setuptools",
+        "setuptools_rust",
+    ],
+    rust_extensions=[
+        rust.RustExtension(
+            "bindings.rust.zbox",
+            "bindings/rust/zbox/Cargo.toml",
+            binding=rust.Binding.PyO3,
+            strip=rust.Strip.Debug,
+        )
+    ]
+)
