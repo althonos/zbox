fa9eb9f932e23ef903309870d21340fd23885388
Martin Larralde <martin.larralde@ens-cachan.fr>
2018-05-02 15:26:30 +0200

    Use precompiled `sccache` in Travis-CI

--- ci/install-sccache.sh
+++ ci/install-sccache.sh
@@ -3,8 +3,15 @@
 set -e
 
 # check if sccache is already installed
-if [ ! -f "$CARGO_HOME/bin/sccache" ]; then
-  RUSTC_WRAPPER="" cargo install sccache --debug
+if [ ! -f "${CARGO_HOME}/bin/sccache" ]; then
+  LATEST=$(cargo search -q sccache | grep sccache | cut -f2 -d"\"")
+  URL="https://github.com/mozilla/sccache/releases/download/${LATEST}/sccache-${LATEST}-x86_64-unknown-linux-musl.tar.gz"
+  curl -SsL $URL | tar xzvC /tmp
+  mv "/tmp/sccache-${LATEST}-x86_64-unknown-linux-musl/sccache" "${CARGO_HOME}/bin/sccache"
 else
   echo 'Using cached sccache binary.'
+fi
+
+if [ ! -d "$SCCACHE_DIR" ]; then
+  mkdir -p "$SCCACHE_DIR"
 fi
--- ci/setup.sh
+++ ci/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 . $TRAVIS_BUILD_DIR/ci/install-rustup.sh
 . $TRAVIS_BUILD_DIR/ci/install-libsodium.sh
-#. $TRAVIS_BUILD_DIR/ci/install-sccache.sh
+. $TRAVIS_BUILD_DIR/ci/install-sccache.sh
